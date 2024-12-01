f6c0dbb90a27cbb2ef502d873e06cfcbdad8696c
Laura <laura.antunes@gmail.com>
2024-12-01 09:07:22 -0300

    alt  pg perfil, inserido snackbar

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useEffect, useState } from 'react';
-import { Container, TextField, Card, CardContent, CardHeader, Grid, Button } from '@mui/material';
+import { Container, TextField, Card, CardContent, CardHeader, Grid, Button, Alert, Snackbar } from '@mui/material';
 import '../../style/Perfil.css';
 import Image from 'next/image';
 import userImage from "../../../public/images/userImg.png";
@@ -15,6 +15,7 @@ const PerfilPage = () => {
     endereco: '',
     profileImage: ''
   });
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // Estado para controlar o Snackbar
   const router = useRouter();
 
   useEffect(() => {
@@ -39,16 +40,24 @@ const PerfilPage = () => {
 
   const enviarFormulario = (event: React.FormEvent) => {
     event.preventDefault();
-    alert("Alterações salvas com sucesso!");
+    setSnackbarOpen(true); // Abre o Snackbar
   };
 
   const alterarSenha = () => {
     router.push('/passwordReset');
   };
 
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false); // Fecha o Snackbar
+  };
+
   return (
     <Layout>
       <Container sx={{ paddingTop: 4 }}>
+        {/* Mensagem fixa no topo */}
+        <Alert severity="info" sx={{ marginBottom: 2 }}>
+          Esta página está em construção. Funcionalidades serão implementadas em breve.
+        </Alert>
         <Card className="perfilCard" variant="outlined" sx={{ boxShadow: "0px 1px 1px rgba(0, 0, 0, 0.45)" }}>
           <CardHeader title="Perfil do Usuário" />
           <CardContent>
@@ -74,11 +83,13 @@ const PerfilPage = () => {
                 onChange={(e) => setUserData({ ...userData, endereco: e.target.value })}
                 required
               />
+
+              <div style={{ marginBottom: '28px' }} /> 
+
               <Grid 
                 container 
                 spacing={{ xs: 2, sm: 2 }} 
-                sx={{ 
-                  mt: 2,
+                sx={{
                   flexDirection: { xs: 'column', sm: 'row' },
                   '& .MuiGrid-item': {
                     width: { xs: '100%', sm: '50%' },
@@ -115,6 +126,17 @@ const PerfilPage = () => {
             </form>
           </CardContent>
         </Card>
+        {/* Snackbar */}
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={3000} // Fecha automaticamente após 3 segundos
+          onClose={handleSnackbarClose}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        >
+          <Alert onClose={handleSnackbarClose} severity="info" sx={{ width: '100%' }}>
+            Estamos trabalhando nessa funcionalidade.
+          </Alert>
+        </Snackbar>
       </Container>
     </Layout>
   );
